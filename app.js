9e22cfa3be71aface3a92ccdb15db42993cdae35
Kelly Illescas <kellyillescas@yahoo.com>
2024-01-14 20:29:10 -0500

    Use MVC pattern to manage application logic

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const hbs = require("hbs");
 
 const indexRouter = require('./app_server/routes/index');
 const usersRouter = require('./app_server/routes/users');
@@ -13,10 +14,11 @@ var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'app_server', 'views'));
-app.set('view engine', 'hbs');
 
 // register handlebars partials (https://www.npmjs.com/package/hbs)
 hbs.registerPartials(path.join(__dirname, 'app_server', 'views/partials'));
+
+app.set('view engine', 'hbs');
 
 app.use(logger('dev'));
 app.use(express.json());
