9258243a789ba6ad6ed786d07cf6a690cac9f0d1
Kelly Illescas <kellyillescas@yahoo.com>
2024-01-15 15:19:21 -0500

    Updatee

--- app_server/controllers/about.js
+++ app_server/controllers/about.js
@@ -1,9 +1,8 @@
-/* GET About View */
-const about = (req, res) => {
-  pageTitle = process.env.npm_package_description + " - About"; // process.env.npm_package_description can only be accessed if you use 'npm start' *nodemon will not work
-  res.render("about", { title: pageTitle });
-};
+/* GET about view */ 
+const about = (req, res) => {    
+  res.render('about', { title: 'Travlr Getaways' }); 
+}; 
 
-module.exports = {
-  about,
-};
+module.exports = {    
+  about 
+}; 
