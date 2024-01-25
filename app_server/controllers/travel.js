3887197a87702eea3399ac0a29f8ff8c976bb27d
Kelly Illescas <kellyillescas@yahoo.com>
2024-01-25 13:14:05 -0500

    Convert travel template from static data to dynamic JSON

--- app_server/controllers/travel.js
+++ app_server/controllers/travel.js
@@ -1,3 +1,6 @@
+const fs = require('fs');
+const trips = JSON.parse(fs.readFileSync('./data/trips.json', 'utf8'));
+
 /* GET Travel View */
 const travel = (req, res) => {
     pageTitle = process.env.npm_package_description + " - Travel";
